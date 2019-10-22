c61cc1607dcce5c4504ac78376765e7bcdae12c1
Raqeebul Ketan <bijoy.ketan@gmail.com>
2019-10-22 00:32:53 -0400

    Implemented base controller

---
+++ src/main/java/com/ketanbd/springbootangularfsapp/Dto/UserDTO.java
@@ -0,0 +1,11 @@
+package com.ketanbd.springbootangularfsapp.Dto;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Users")
+public class UserDTO {
+
+
+}
